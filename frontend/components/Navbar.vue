e7b342fa2166392a7275fc202a348b0f972d2321
Rian <riansouza0@gmail.com>
2024-05-27 21:52:03 -0300

    feat: navbar creation

---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <ClientOnly>
+    <div
+      class="bg-[#057D88] w-full h-16 px-6 flex items-center justify-between"
+    >
+      <img src="~/assets/CredifitLogo.png" alt="Logo" class="" />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div class="flex justify-center items-center gap-2">
+            <Icon name="iconamoon:profile" color="white" class="size-6" />
+
+            <p class="text-white font-bold">Diego Viana</p>
+            <Icon name="mingcute:down-fill" color="white" class="size-5" />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  </ClientOnly>
+</template>
+
+<script>
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+export default {
+  name: "Navbar",
+};
+</script>
