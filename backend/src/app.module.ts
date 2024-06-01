2290690ffd1d12b3f54b0af21e05208ca49b5037
Rian <riansouza0@gmail.com>
2024-06-01 03:46:04 -0300

    feat: company crud

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { EmployeeModule } from "./employee/employee.module";
 import { PrismaService } from "./prisma.service";
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [EmployeeModule, CompanyModule],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- backend/src/company/company.service.ts
+++ backend/src/company/company.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
-import { CreateEmployeeDto } from "./dto/create-employee.dto";
-import { UpdateEmployeeDto } from "./dto/update-employee.dto";
+import { CreateCompanyDto } from "./dto/create-company.dto";
+import { UpdateCompanyDto } from "./dto/update-company.dto";
 import { PrismaService } from "src/prisma.service";
 
 @Injectable()
-export class EmployeeService {
+export class CompanyService {
   constructor(private prisma: PrismaService) {}
 
-  private async isUniqueEmail(data: CreateEmployeeDto | UpdateEmployeeDto) {
+  private async isUniqueEmail(data: CreateCompanyDto | UpdateCompanyDto) {
     const email = await this.prisma.employee.findUnique({
       where: { email: data["email"] },
     });
@@ -15,7 +15,7 @@ export class EmployeeService {
       throw new NotFoundException([`Este email já existe!`]);
     }
   }
-  private async isUniqueCpf(data: CreateEmployeeDto | UpdateEmployeeDto) {
+  private async isUniqueCpf(data: CreateCompanyDto | UpdateCompanyDto) {
     const cpf = await this.prisma.employee.findUnique({
       where: { cpf: data["cpf"] },
     });
@@ -29,41 +29,40 @@ export class EmployeeService {
       throw new NotFoundException(["o id deve ser um número!"]);
     }
   }
-
-  async create(data: CreateEmployeeDto) {
+  async create(data: CreateCompanyDto) {
     await this.isUniqueEmail(data);
     await this.isUniqueCpf(data);
 
-    return await this.prisma.employee.create({ data });
+    return await this.prisma.company.create({ data });
   }
 
   async findAll() {
-    return await this.prisma.employee.findMany();
+    return await this.prisma.company.findMany();
   }
 
   async findOne(id: number) {
     await this.validateId(id);
-    const employee = await this.prisma.employee.findUnique({ where: { id } });
-    if (!employee) {
+    const company = await this.prisma.company.findUnique({ where: { id } });
+    if (!company) {
       throw new NotFoundException(["Este id não foi encontrado!"]);
     }
-    return employee;
+    return company;
   }
 
-  async update(id: number, data: UpdateEmployeeDto) {
+  async update(id: number, data: UpdateCompanyDto) {
     await this.validateId(id);
     await this.isUniqueEmail(data);
     await this.isUniqueCpf(data);
 
-    const employee = await this.prisma.employee.update({ where: { id }, data });
-    if (!employee) {
+    const company = await this.prisma.company.update({ where: { id }, data });
+    if (!company) {
       throw new NotFoundException(["Este id não foi encontrado!"]);
     }
-    return employee;
+    return company;
   }
 
   async remove(id: number) {
     await this.validateId(id);
-    return await this.prisma.employee.delete({ where: { id } });
+    return await this.prisma.company.delete({ where: { id } });
   }
 }
